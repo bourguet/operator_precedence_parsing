8576cca0866ff341a58ad9b9a8796032db43f3de
Jean-Marc Bourguet <jm@bourguet.org>
2020-09-01 22:37:31 +0200

    Handling of exception during tests.

--- shunting_yard_tests.py
+++ shunting_yard_tests.py
@@ -7,10 +7,20 @@ import jmb_cexp_tests
 
 def check_parsing(s, expected):
     p = shunting_yard.cexp_parser()
-    tree = p.parse(s)
-    sexpr = repr(tree)
-    if sexpr != expected:
-        print('Failed: {} => {} != {}'.format(s, sexpr, expected))
+    try:
+        tree = p.parse(s)
+        sexpr = repr(tree)
+        if sexpr != expected:
+            if expected == '':
+                print('Failing to parse: {} => {}'.format(s, sexpr))
+            else:
+                print('UNEXPECTED Failure to parse: {} => {} != {}'.format(s, sexpr, expected))
+    except RuntimeError as error:
+        if expected == '':
+            print('Exception while parsing {} => {}'.format(s, error))
+        else:
+            print('UNEXPECTED exception while parsing: {} => {}, expected {}'.format(s, error, expected))
+
 
 andychu_cexp_tests.all(check_parsing)
 jmb_cexp_tests.all_tests(check_parsing)
