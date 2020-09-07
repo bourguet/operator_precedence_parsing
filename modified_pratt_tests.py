a516288ddc71cd91b0a155fd63303b70405d33ac
Jean-Marc Bourguet <jm@bourguet.org>
2020-09-07 16:30:26 +0200

    Rename pratt to modified_pratt as I'm not sure this is the true Pratt algorithm. I intend now to do with Pratt what I've done with Dijkstra and Knuth, a from scratch implementation using only paper.

--- modified_pratt_tests.py
+++ modified_pratt_tests.py
@@ -1,16 +1,17 @@
 #! /usr/bin/env python3
 
-import pratt
+import modified_pratt
 import andychu_cexp_tests
 import jmb_cexp_tests
 
 
 def check_parsing(s, expected):
-    p = pratt.cexp_parser()
+    p = modified_pratt.cexp_parser()
     tree = p.parse(s)
     sexpr = repr(tree)
     if sexpr != expected:
         print('Failed: {} => {} != {}'.format(s, sexpr, expected))
+
 
 andychu_cexp_tests.all(check_parsing)
 jmb_cexp_tests.all_tests(check_parsing)
